55b12fd3b5029dfd90233c9d0346aed9d6c90283
dharmasam <kumar.sumit2512@gmail.com>
2024-06-21 22:17:00 +0530

    namaste-react

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1", { id: "heading" }, "This is done by React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+
+
+
+// const parent = React.React
